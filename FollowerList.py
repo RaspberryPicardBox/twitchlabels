937cbea86d6928eaa2f1510cc12954644e5c2032
Michael Boyce <raspberrypicardbox@gmail.com>
2024-12-14 17:58:39 +0000

    Updated to satisfy the more recent OAuth Twitch requirements.
    - Replaced the request-based API integration with 'twitchAPI'
    - Added clientSecret to the creds.json
    - Added a requirements.txt to include twitchAPI

--- FollowerList.py
+++ FollowerList.py
@@ -1,149 +1,102 @@
-from requests import Session
-from requests.adapters import HTTPAdapter
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.type import AuthScope
+from twitchAPI.helper import first
 from pathlib import Path
 import json
+import asyncio
 
 ########################
 #  User Defined Values #
 ########################
-# If you don't know Python, booleans (yes or no) need to be set as True or
-# False (with the first letter as capital)
 userSettings = {
     "recent_followers": {
-        # How many recent follower's you've had. Leave at zero to load the
-        # default amount
-        "limit": 0,
-
-        # If the list should be reversed
-        "reverse": False
+        "limit": 0,  # Number of recent followers to retrieve. Set to 0 for default amount.
+        "reverse": False  # Set to True to reverse the order of the follower list.
     }
 }
 
 ##################################################################
-#  Unless you know what you're doing, don't touch the code below #
+#  Credential Checks and Initialization  #
 ##################################################################
-# Checks if the credentials file is present
+# Check if the credentials file is present
 credentialsFile = Path("creds.json")
-if not (credentialsFile.exists()) & (credentialsFile.is_file()):
+if not (credentialsFile.exists() and credentialsFile.is_file()):
     print("Please set up the credentials file and then re-run this program")
     exit()
 
-# Load up the credentials file and check if the credentials data is all entered
+# Load the credentials file and validate the data
 credJsonFile = json.load(open(credentialsFile, 'r'))
-if (credJsonFile["userName"] == ""):
-    print("Oops, looks like you forgot to enter in your username")
+if not credJsonFile.get("userName"):
+    print("Oops, looks like you forgot to enter your username")
     exit()
-
-if (credJsonFile["credentials"]["clientId"] == ""):
-    print("Oops, looks like you forgot to enter in your client ID")
+if not credJsonFile["credentials"].get("clientId"):
+    print("Oops, looks like you forgot to enter your client ID")
     exit()
 
 userName = credJsonFile["userName"]
 credentials = credJsonFile["credentials"]
+client_id = credentials['clientId']
+client_secret = credentials['clientSecret']
 
-def twitchApi(query, parameter, values, limit = 0):
-    # Twitch's base API url ("Helix" is essentially v6)
-    baseApiUrl = "https://api.twitch.tv/helix/"
+target_scope = [AuthScope.MODERATOR_READ_FOLLOWERS]
 
-    # If it is a single value
-    apiQueryUrl = ""
-    if (type(values) is str):
-        value = values
-        apiQueryUrl = baseApiUrl + query + "?" + parameter + "=" + value
-
-    # If it's an array/list
-    else:
-        arguments = ""
-        for value in values:
-            arguments = arguments + parameter + "=" + value + "&"
-
-        apiQueryUrl = baseApiUrl + query + "?" + arguments
-        apiQueryUrl = apiQueryUrl.rstrip("&")
-
-    # If a limit number is given
-    if (limit > 0):
-        apiQueryUrl = apiQueryUrl + "&first=" + str(limit)
-        print(apiQueryUrl)
-
-    retryAdapter = HTTPAdapter(max_retries = 2)
-    session = Session()
-    session.mount('https://', retryAdapter)
-    session.mount('http://', retryAdapter)
-
-    # Run the API request, and store the results
-    response = session.get(apiQueryUrl, headers = {
-        'Client-ID': credentials['clientId'],
-        'Content-Type': 'application/json'
-    })
-
-    # Check that the value was fetched and set correctly
-    try:
-        result = json.loads(response.text)
-
-    except:
-        print("Was unable to fetch value. Exiting...")
-        exit()
-
-    # If something was set for the result variable, continue. If nothing, exit
-    if not (result):
-        print("Unable to set value. Exiting...")
-        exit()
-
-    # Checks if an error was returned
-    if 'status' in result:
-        print("API ERROR OCCURED:")
-        print("")
-        if (result['status'] == 429):
-            print("Status ID: 429")
-        print("Error Name: " + result["error"])
-        print("Error Message: " + result["message"])
-        print("")
-        print(result)
-        exit()
-
-    return result['data']
 
 def nameToFile(userNames, fileName):
-    file = open(fileName + ".txt","w")
+    """
+    Writes the provided user names to a specified file.
+    If a single user name is provided, it writes it directly.
+    If a list is provided, it writes each name on a new line.
+    """
+    with open(fileName + ".txt", "w") as file:
+        if isinstance(userNames, str):
+            file.write(userNames)
+        else:
+            for userName in userNames:
+                file.write(userName + "\n")
 
-    # If it is a single value
-    if (type(userNames) is str):
-        userName = userNames
-        file.write(userName)
+async def authenticate_twitch():
+    """
+    Authenticates the Twitch API using user credentials.
+    Returns the authenticated Twitch instance.
+    """
+    twitch = Twitch(client_id, client_secret)
+    auth = UserAuthenticator(twitch, target_scope, force_verify=False)
+    token, refresh_token = await auth.authenticate()
+    await twitch.set_user_authentication(token, target_scope, refresh_token)
+    return twitch
 
-    # If it's an array/list
-    else:
-        for userName in userNames:
-            file.write(userName + "\n")
+async def main(twitch):
+    """
+    Main function to retrieve and process follower data.
+    Uses the authenticated Twitch instance to get the user ID and follower list.
+    """
+    user = await first(twitch.get_users(logins=[userName]))
+    userId = user.id
 
-    file.close()
+    followers = await twitch.get_channel_followers(broadcaster_id=userId)
+    ordered_followers = []
 
-    return
+    async for follower in followers:
+        ordered_followers.append(follower.user_name)
 
-# Grab the user's user_id
-userId = twitchApi("users", "login", userName)[0]['id']
+    if userSettings["recent_followers"]["reverse"]:
+        ordered_followers.reverse()
 
-# Grab the follower list
-followerList = twitchApi("users/follows", "to_id", userId, userSettings['recent_followers']['limit'])
+    if userSettings["recent_followers"]["limit"] > 0:
+        ordered_followers = ordered_followers[:userSettings["recent_followers"]["limit"]]
 
-# Most recent follower
-followerUserId = followerList[0]["from_id"]
-displayName = twitchApi("users", "id", followerUserId)[0]['display_name']
-nameToFile(displayName, "newest_follower")
+    nameToFile(ordered_followers, "recent_followers")
+    nameToFile(ordered_followers[0], "newest_follower")
 
-if (userSettings['recent_followers']['reverse']):
-    followerList = reversed(followerList)
-
-# 20 recent followers
-followerIds = []
-for follower in followerList:
-    followerUserId = follower["from_id"]
-    followerIds.append(followerUserId)
-
-followersInfo = twitchApi("users", "id", followerIds)
-displayNames = []
-for follower in followersInfo:
-    displayNames.append(follower["display_name"])
-
-nameToFile(displayNames, "recent_followers")
-
+if __name__ == "__main__":
+    print("FollowerList is running. Press Ctrl+C to stop.")
+    loop = asyncio.new_event_loop()
+    try:
+        twitch = loop.run_until_complete(authenticate_twitch())
+        while True:
+            loop.run_until_complete(main(twitch))
+            loop.run_until_complete(asyncio.sleep(5))
+    except (KeyboardInterrupt, SystemExit):
+        print("Closing FollowerList.")
+        loop.close()
